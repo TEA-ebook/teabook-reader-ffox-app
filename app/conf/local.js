b05727814a7aa8077e4cd961a7b1b53c07a30879
Johan Poirier <johan.poirier@gmail.com>
2015-04-14 16:55:41 +0200

    Update main color and icon

--- app/conf/local.js
+++ app/conf/local.js
@@ -1,4 +1,4 @@
 var Conf = {};
-Conf.host = 'http://tea-reader-engine.local';
+Conf.host = 'http://www.tea-reader-engine.com';
 Conf.logsEndPoint = 'http://192.168.10.6:1339';
 Conf.website = 'http://pp-www.tea-reader-app.com';
