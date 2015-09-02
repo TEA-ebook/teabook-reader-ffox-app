27ec9332e111e4f938948416b7912516aff9a8d9
Johan Poirier <johan.poirier@gmail.com>
2015-09-02 14:00:35 +0200

    Add: ukrainian file for localization

--- app/conf/local.js
+++ app/conf/local.js
@@ -1,4 +1,4 @@
 var Conf = {};
-Conf.host = 'http://www.tea-reader-engine.com';
+Conf.host = 'http://192.168.10.6:1339';
 Conf.logsEndPoint = 'http://192.168.10.6:1339';
 Conf.website = 'http://pp-www.tea-reader-app.com';
